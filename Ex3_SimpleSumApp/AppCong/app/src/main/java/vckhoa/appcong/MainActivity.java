351da556d892547868a7ca8d5469f7c6e851a488
Chi Khoa <111358604+vckhoa1214@users.noreply.github.com>
2024-03-07 07:22:14 +0700

    Update Ex3

--- Ex3_SimpleSumApp/AppCong/app/src/main/java/vckhoa/appcong/MainActivity.java
+++ Ex3_SimpleSumApp/AppCong/app/src/main/java/vckhoa/appcong/MainActivity.java
@@ -1,24 +1,29 @@
 package vckhoa.appcong;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    public void XuLyCong(View view){
+        EditText edtA = findViewById(R.id.edtA);
+        EditText edtB = findViewById(R.id.edtB);
+        EditText edtKQ = findViewById(R.id.edtKQ);
+
+        double a = Double.parseDouble(edtA.getText().toString());
+        double b = Double.parseDouble(edtB.getText().toString());
+
+        double tong = a + b;
+
+        edtKQ.setText(String.valueOf(tong));
+
     }
 }
