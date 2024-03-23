886e30203ab73762bebef2fe9fec599cff1f9ff8
Chi Khoa <111358604+vckhoa1214@users.noreply.github.com>
2024-03-23 14:30:22 +0700

    Update BMICalculator.java

--- Cau1_BMICalculator/src/ntu_63134338/BMICalculator.java
+++ Cau1_BMICalculator/src/ntu_63134338/BMICalculator.java
@@ -1,19 +1,20 @@
 package ntu_63134338;
 
-import java.awt.EventQueue;
-
+import java.awt.Font;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class BMICalculator extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
