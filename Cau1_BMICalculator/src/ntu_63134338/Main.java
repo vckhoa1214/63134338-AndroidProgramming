2c16d9ea6336c067f4af920fd9fb92fd8fe504fc
Chi Khoa <111358604+vckhoa1214@users.noreply.github.com>
2024-03-23 14:37:08 +0700

    Create Main.java

---
+++ Cau1_BMICalculator/src/ntu_63134338/Main.java
@@ -0,0 +1,8 @@
+package ntu_63134338;
+
+public class Main {
+	public static void main(String[] args) {
+        BMICalculator frame = new BMICalculator();
+        frame.setVisible(true);
+    }
+}
